58688524d65b0f90ccffaf7e1edbfaefd72ef354
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-10-31 12:01:21 +0500

    Add new Methods

--- DapperMethods/Controllers/UsersController.cs
+++ DapperMethods/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using Dapper;
+using DapperMethods.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Data.SqlClient;
 
 namespace DapperMethods.Controllers
@@ -48,5 +50,123 @@ namespace DapperMethods.Controllers
                 return Ok(count);
             }
         }
+        [HttpGet("ExecuteScalarAsyncGeneric")]
+        public async Task<ActionResult> GetExecuteScalarAsyncGeneric()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sql = "SELECT COUNT(*) FROM Test";
+                var count = await connection.ExecuteScalarAsync<int>(sql);
+
+                Console.WriteLine($"Total products: {count}");
+                return Ok(count);
+            }
+        }
+        [HttpGet("QuerySingle")]
+        public IActionResult GetQuerySingle()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sql = "SELECT * FROM Test WHERE id = 1";
+                var product = connection.QuerySingle(sql);
+
+                Console.WriteLine($"{product.id} {product.name}");
+                return Ok(product);
+            }
+        }
+        [HttpGet("QuerySingleGeneric")]
+        public IActionResult GetQuerySingleGeneric()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sql = "SELECT * FROM Test WHERE id = 2";
+                var product = connection.QuerySingle<string>(sql);
+
+                
+                return Ok(product);
+            }
+        }
+        [HttpGet("QuerySingleAsync")]
+        public async Task<ActionResult> GetQuerySingleAsync()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT * FROM Test WHERE id = 2";
+                var customer = await connection.QuerySingleAsync(sql);
+
+                return Ok(customer);
+            }
+        }
+
+        [HttpGet("QuerySingleAsyncGeneric")]
+        public async Task<ActionResult> GetQuerySingleAsyncGeneric()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sql = "SELECT * FROM Test WHERE id = 1";
+                var customer = await connection.QuerySingleAsync<User>(sql);
+
+                Console.WriteLine($"{customer.name} {customer.age}");
+                return Ok(customer);
+            }
+        }
+
+        [HttpGet("QueryFirst")]
+        public IActionResult QueryFirst()
+        {
+            string sql = "SELECT * FROM Test WHERE id=2";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var customer = connection.QueryFirst<User>(sql);
+                Console.WriteLine(customer.name);
+                Console.WriteLine(customer.age);
+                return Ok(customer);
+            }
+        }
+
+        [HttpGet("QueryFirstAsync")]
+        public async Task<ActionResult> QueryFirstAsync()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sql = "SELECT * FROM Test WHERE id = 1";
+                var customer = await connection.QueryFirstAsync<User>(sql);
+
+                Console.WriteLine($"{customer.name} {customer.age}");
+                return Ok(customer);
+            }
+        }
+
+        [HttpGet("Query")]
+        public IActionResult Query()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sql = "SELECT * FROM Test";
+                var customers = connection.Query(sql);
+
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine($"{customer.name} {customer.age}");
+                }
+                return Ok(customers);
+            }
+        }
+        [HttpGet("QueryAsync")]
+        public async Task<ActionResult> QueryAsync()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sql = "SELECT * FROM Test";
+                var customers = await connection.QueryAsync(sql);
+
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine($"{customer.CustomerID} {customer.CompanyName}");
+                }
+                return Ok(customers);
+            }
+        }
+
     }
 }
---
+++ DapperMethods/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace DapperMethods.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
